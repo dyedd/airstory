eccd87a05cafd7514ffb1819fb7d2dedbd7ef0c2
dyedd <1176996982@qq.com>
2021-05-23 16:18:49 +0800

    feat: 天气预报功能搭建完毕

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,16 +1,20 @@
 // index.js
 // 获取应用实例
+import utils from '../../utils/util';
 const app = getApp()
-
+const API_KEY = 'cc5e9d1087ca4a4bbbdc72f5ce990ca7'
 Page({
   data: {
     active: 0,
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    icon:{
+      weatherNormal:"cloud://airstory-2gfltwcp7e973deb.6169-airstory-2gfltwcp7e973deb-1305977660/weather_normal.png",
+      weatherSelect:"cloud://airstory-2gfltwcp7e973deb.6169-airstory-2gfltwcp7e973deb-1305977660/weather_select.png",
+      qualityNormal:"cloud://airstory-2gfltwcp7e973deb.6169-airstory-2gfltwcp7e973deb-1305977660/quality_normal.png",
+      qualitySelect:"cloud://airstory-2gfltwcp7e973deb.6169-airstory-2gfltwcp7e973deb-1305977660/quality_select.png",
+      adviceNormal:"cloud://airstory-2gfltwcp7e973deb.6169-airstory-2gfltwcp7e973deb-1305977660/advice_normal.png",
+      adviceSelect:"cloud://airstory-2gfltwcp7e973deb.6169-airstory-2gfltwcp7e973deb-1305977660/advice_select.png"
+
+    },
   },
   onChange(event) {
     // event.detail 的值为当前选中项的索引
@@ -23,31 +27,104 @@ Page({
     })
   },
   onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
+    let that = this;
+    that.getLocation()
   },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+  getLocation(){
+    let that = this;
+    wx.getLocation({
+      type: 'wgs84',
+      success (res) {
+        const latitude = res.latitude
+        const longitude = res.longitude
+        that.getCity(`${longitude},${latitude}`)
+      }
+     })
+  },
+  getCity(location){
+    let that=this;
+    wx.request({
+      url: 'https://geoapi.qweather.com/v2/city/lookup',
+      data: {
+        'key': API_KEY,
+        'location': location
+      },
+      success(res){
+        // console.log(res.data)
+        that.setData({
+          locationName: res.data.location[0].name,
+          adm2:res.data.location[0].adm2,
+        })
+        that.getNowWeather(res.data.location[0].id)
+        that.get24Weather(res.data.location[0].id)
+        that.get7dWeather(res.data.location[0].id)
+      }
+    })
+  },
+  getNowWeather(location){
+    let that = this;
+    wx.request({
+      url: 'https://devapi.qweather.com/v7/weather/now',
+      data: {
+        'key': API_KEY,
+        'location': location
+      },
+      success(res){
+        // console.log(res.data)
+        that.setData({
+          now:{
+            temp: res.data.now.temp,
+            text: res.data.now.text,
+            icon: res.data.now.icon,
+            windDir: res.data.now.windDir,
+            windScale: res.data.now.windScale,
+            feelsLike: res.data.now.feelsLike,
+            humidity: res.data.now.humidity,
+          }
         })
       }
     })
   },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+  get24Weather(location) {
+    let that = this;
+    wx.request({
+      url: 'https://devapi.qweather.com/v7/weather/24h',
+      data: {
+        'key': API_KEY,
+        'location': location
+      },
+      success(res){
+        console.log(res.data)
+        res.data.hourly.forEach((item)=>{
+          item.time = utils.formatTime(new Date(item.fxTime)).hourly
+        })
+        that.setData({
+          hourly: res.data.hourly
+        })
+      }
     })
+  },
+  get7dWeather(location){
+    let that = this;
+    wx.request({
+      url: 'https://devapi.qweather.com/v7/weather/7d',
+      data: {
+        'key': API_KEY,
+        'location': location
+      },
+      success(res){
+        res.data.daily.forEach((item)=>{
+          item.date = utils.formatTime(new Date(item.fxDate)).daily
+          item.dateToString = utils.formatTime(new Date(item.fxDate)).dailyToString
+          item.text = item[`text${utils.isDayOrNight(item.sunset)}`]
+          item.icon = item[`icon${utils.isDayOrNight(item.sunset)}`]
+        })
+        that.setData({
+          daily: res.data.daily
+        })
+        console.log(res.data)
+      }
+    })
+    
   }
 })
--- utils/util.js
+++ utils/util.js
@@ -1,19 +1,29 @@
 const formatTime = date => {
-  const year = date.getFullYear()
+  // const year = date.getFullYear()
   const month = date.getMonth() + 1
   const day = date.getDate()
   const hour = date.getHours()
   const minute = date.getMinutes()
-  const second = date.getSeconds()
-
-  return `${[year, month, day].map(formatNumber).join('/')} ${[hour, minute, second].map(formatNumber).join(':')}`
+  // const second = date.getSeconds()
+  const weekArray = ["周日", "周一", "周二", "周三", "周四", "周五", "周六"]
+  const isToday = date.setHours(0, 0, 0, 0) == new Date().setHours(0, 0, 0, 0)
+  return {
+    hourly: [hour, minute].map(formatNumber).join(":"),
+    daily: [month, day].map(formatNumber).join("-"),
+    dailyToString: isToday ? "今天" : weekArray[date.getDay()]
+  }
 }
-
+const isDayOrNight = date =>{
+  const hour = date.split(":")[0]
+  const minute = date.split(":")[1]
+  return new Date().setHours(hour,minute) >= new Date().getTime()?"Day":"Night"
+}
 const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : `0${n}`
 }
 
 module.exports = {
-  formatTime
+  formatTime,
+  isDayOrNight
 }
